d139d90a56f7836b6aaaabf4fd7d13d525103e47
replayzor <oltean.ionut1989@gmail.com>
2023-02-11 15:22:01 +0200

    Add isValidUrl and functinality for adding new facts

--- src/components/NewFactForm.jsx
+++ src/components/NewFactForm.jsx
@@ -1,15 +1,51 @@
 import { useState } from "react";
 import CATEGORIES from "../data";
 
-function NewFactForm() {
+function NewFactForm({ showFacts, setShowForm }) {
 	const [text, setText] = useState("");
-	const [source, setSource] = useState("");
+	const [source, setSource] = useState("http://example.com");
 	const [category, setCategory] = useState("");
 	const textLength = text.length;
 
+	const isValidHttpUrl = (string) => {
+		let url;
+		try {
+			url = new URL(string);
+		} catch (_) {
+			return false;
+		}
+		return url.protocol === "http:" || url.protocol === "https:";
+	};
+
 	const handleSubmit = (e) => {
+		// 1. Prevent browser reload
 		e.preventDefault();
 		console.log(text, source, category);
+
+		// 2. Check if data is valid. If so create a new fact
+		if (text && isValidHttpUrl(source) && category && textLength <= 200) {
+			console.log("this is valid data");
+
+			// 3. Create a new fact object
+			const newFact = {
+				id: Math.round(Math.random() * 1000000),
+				text,
+				source,
+				category,
+				votesInteresting: 0,
+				votesMindblowing: 0,
+				votesFalse: 0,
+				createdIn: new Date().getFullYear(),
+			};
+			// 4. Add the new fac to the UI: add the fact to state
+			showFacts((currentFacts) => [newFact, ...currentFacts]);
+			// 5. Reset input fields
+			setText("");
+			setSource("");
+			setCategory("");
+			// 6. Close the form
+			setShowForm(false);
+		}
 	};
 
 	return (
