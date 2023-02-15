f195acc2205ee82df31aaf0643fdcb62c939ab99
replayzor <oltean.ionut1989@gmail.com>
2023-02-15 18:00:53 +0200

    Add state .loading compoenent + supabase

--- src/App.jsx
+++ src/App.jsx
@@ -8,53 +8,34 @@ import CategoryFilter from "./components/CategoryFilter";
 import NewFactForm from "./components/NewFactForm";
 import FactList from "./components/FacList";
 import Header from "./components/Header";
-
-const initialFacts = [
-	{
-		id: 1,
-		text: "React is being developed by Meta (formerly Facebook)",
-		source: "https://opensource.fb.com/",
-		category: "technology",
-		votesInteresting: 24,
-		votesMindblowing: 9,
-		votesFalse: 4,
-		createdIn: 2021,
-	},
-	{
-		id: 2,
-		text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-		source:
-			"https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-		category: "society",
-		votesInteresting: 11,
-		votesMindblowing: 2,
-		votesFalse: 0,
-		createdIn: 2019,
-	},
-	{
-		id: 3,
-		text: "Lisbon is the capital of Portugal",
-		source: "https://en.wikipedia.org/wiki/Lisbon",
-		category: "society",
-		votesInteresting: 8,
-		votesMindblowing: 3,
-		votesFalse: 1,
-		createdIn: 2015,
-	},
-];
+import Loading from "./components/Loading";
 
 function App() {
 	const [showForm, setShowForm] = useState(false);
 	const [facts, setFacts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [currentCategory, setCurrentCategory] = useState("all");
 
 	useEffect(() => {
 		const getFacts = async () => {
-			const { data: facts, error } = await supabase.from("facts").select("*");
-			setFacts(facts);
-			console.log(facts, error);
+			setIsLoading(true);
+
+			let query = supabase.from("facts").select("*");
+
+			if (currentCategory !== "all")
+				query = query.eq("category", currentCategory);
+
+			const { data: facts, error } = await query
+				.order("votesInteresting", { ascending: false })
+				.limit(1000);
+
+			if (!error) setFacts(facts);
+			else alert(error.message);
+
+			setIsLoading(false);
 		};
 		getFacts();
-	}, []);
+	}, [currentCategory]);
 
 	const clickFormHandler = () => {
 		setShowForm((show) => !show);
@@ -68,8 +49,12 @@ function App() {
 			) : null}
 
 			<main className="main">
-				<CategoryFilter />
-				<FactList facts={facts} />
+				<CategoryFilter setCurrentCategory={setCurrentCategory} />
+				{isLoading ? (
+					<Loading />
+				) : (
+					<FactList facts={facts} setFacts={setFacts} />
+				)}
 			</main>
 		</>
 	);
