b1b5811ea91a6f2bd174e086c3f0c9b8c9bf291a
replayzor <oltean.ionut1989@gmail.com>
2023-02-15 18:02:21 +0200

    Add filter functionality by categories

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,15 +1,22 @@
 import CATEGORIES from "../data";
 
-function CategoryFilter() {
+function CategoryFilter({ setCurrentCategory }) {
+	const clickAllHandler = () => {
+		setCurrentCategory("all");
+	};
+
 	return (
 		<aside>
 			<ul>
 				<li className="category">
-					<button className="btn btn-all-categories">All</button>
+					<button className="btn btn-all-categories" onClick={clickAllHandler}>
+						All
+					</button>
 				</li>
 				{CATEGORIES.map((cat) => (
 					<li key={cat.name} className="category">
 						<button
+							onClick={() => setCurrentCategory(cat.name)}
 							className="btn btn-category"
 							style={{ backgroundColor: cat.color }}
 						>
